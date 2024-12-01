91e29c602eb8ddbaf6ab927a2444dc6f6f2e9bbb
Alexandre Lepretre <alexandre.lprtr@gmail.com>
2024-12-01 13:39:09 -0500

    Improve code
    Yeah I use ChatGPT to understand how I can do things in a better way
    (but you know what? I don't care I'm learning).

--- 2024/day01/src/main.rs
+++ 2024/day01/src/main.rs
@@ -25,7 +25,7 @@ fn part_two() -> u32 {
     let mut id_values = HashMap::<u32, u32>::new();
 
     for id in right {
-        id_values.entry(id).and_modify(|x| *x += 1).or_insert(1);
+        *id_values.entry(id).or_insert(0) += 1;
     }
 
     left.iter()
@@ -34,21 +34,17 @@ fn part_two() -> u32 {
 }
 
 fn get_inputs() -> (Vec<u32>, Vec<u32>) {
-    let inputs = read_file_inputs("./inputs/day01.txt");
-    let mut left = Vec::with_capacity(inputs.lines().count());
-    let mut right = Vec::with_capacity(inputs.lines().count());
+    read_file_inputs("./inputs/day01.txt")
+        .lines()
+        .map(|line| {
+            let numbers : Vec<u32> = line
+                .split_whitespace()
+                .map(|x| x.parse::<u32>().unwrap())
+                .collect();
 
-    for line in inputs.lines() {
-        let numbers : Vec<u32> = line
-            .split_whitespace()
-            .map(|x| x.parse::<u32>().unwrap())
-            .collect();
-
-        left.push(numbers[0]);
-        right.push(numbers[1])
-    }
-
-    (left, right)
+            (numbers[0], numbers[1])
+        })
+    .unzip()
 }
 
 fn read_file_inputs(path: &str) -> String {
